de02258ae7e4aa2e884fb7a975bf191b84af4f2e
Kyle J. McKay <mackyle@gmail.com>
2011-11-11 18:35:26 -0800

    Add Spanish localization courtesy of Javier Tamames

---
+++ widget/localizations/es.lproj/LocalizedStrings.js
@@ -0,0 +1,24 @@
+﻿//
+//  LocalizedStrings.js (es)
+//  Translation by Javier Tamames
+//
+
+localizedStrings = new Array();
+
+localizedStrings['Dawn']         = 'Alba';
+localizedStrings['Sunrise']      = 'Orto';
+localizedStrings['Sunset']       = 'Ocaso';
+localizedStrings['Dusk']         = 'Crepúsculo';
+
+localizedStrings['Country:']     = 'País:';
+localizedStrings['State:']       = 'Estado:';
+localizedStrings['City:']        = 'Ciudad:';
+
+localizedStrings['Twilight:']    = 'Crepúsculo:';
+localizedStrings['Civil']        = 'Civil';
+localizedStrings['Nautical']     = 'Náutico';
+localizedStrings['Astronomical'] = 'Astronómico';
+
+localizedStrings['by']           = 'de';
+localizedStrings['About']        = 'Acerca de';
+localizedStrings['Done']         = 'Hecho';
